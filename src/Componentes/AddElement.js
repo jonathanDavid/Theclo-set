fa84d12f0797d24131280faff1b8d749659cf1a9
johan <jdavid.ilias@gmail.com>
2018-10-31 12:27:54 -0500

    Camera

---
+++ src/Componentes/AddElement.js
@@ -0,0 +1,153 @@
+/*React*/
+import React,   { Component } from 'react';
+import { StyleSheet,StatusBar,View,Image } from 'react-native';
+/*Components*/
+import { Container,Card,Label,Header,Form,Content,Button,Text,CardItem,Item,Body,Title,Input,Icon,Right,Left } from 'native-base';
+
+
+
+export default class AddElement extends Component {
+  constructor(props){
+    super(props);
+    state={Name:'',Description:'',Photo:''};
+  }
+
+  onPressBack = ()=>{
+    this.props.onPressBack();
+  }
+
+  componentWillMount(){
+    let data = this.props.dataEdit;
+    if(data){
+      this.setState({Name: data.Nombre, Description: data.Descripcion, Photo:data.Photo});
+    }else{
+      this.setState({Name:'',Description:'',Photo:null});
+    }
+  }
+
+  returnData(uri) {
+    this.setState({Photo: uri});
+  }
+
+  OpenCamera=()=>{
+    this.props.OpenCamera(this.returnData.bind(this))
+  }
+
+  renderImageCamera=()=>{
+    let code= [];
+
+    if(this.state.Photo !== null){
+      code.push(
+        <Body style={{ flex: 1,alignItems: 'center', flexDirection: 'row',justifyContent: 'center', height: 200  }}>
+         <Image source={{uri: this.state.Photo}} style={{height: 200, width: null, flex: 1}}/>
+        </Body>
+      )
+    }else{
+      code.push(
+        <Body style={{ flex: 1,alignItems: 'center', flexDirection: 'row',justifyContent: 'center', height: 200  }}>
+          <Icon type="FontAwesome" name='camera-retro'/>
+        </Body>
+      )
+    }
+
+    return code
+  }
+
+  addNew=()=>{
+    let data ={Nombre: this.state.Nombre, Description: this.state.Descripcion, Photo: this.state.Photo}
+    this.props.addNew(data);
+  }
+
+  render() {
+    return (
+      <Container>
+        <Header style={{backgroundColor: '#6432c8'}}>
+          <StatusBar backgroundColor={'#6432c8'} barStyle="light-content"/>
+          <Left>
+            <Button onPress={this.onPressBack} transparent>
+              <Icon name='arrow-back' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>{this.props.Title}</Title>
+          </Body>
+        </Header>
+          <Content>
+            <Form>
+              <Card>
+                <CardItem cardBody button onPress={this.OpenCamera}>
+                  {this.renderImageCamera()}
+                </CardItem>
+              </Card>
+                <Item style ={styles.inputLayout} floatingLabel require>
+                  <Label>Nombre</Label>
+                  <Input value={this.state.Name} onChangeText={(info) => {this.setState({Name:info})}}/>
+                </Item>
+                <Item style ={styles.inputLayout} floatingLabel require>
+                  <Label>Descripcion</Label>
+                  <Input value={this.state.Description} onChangeText={(info) => {this.setState({Description:info})}}/>
+                </Item>
+                <View style ={styles.buttonView}>
+                  <Button onPress= {this.addNew} style={styles.buttonLayoutBottom} >
+                    <Text style={{color: 'white',fontWeight: 'bold',fontSize: 16}}>Guardar</Text>
+                  </Button>
+                </View>
+            </Form>
+          </Content>
+        </Container>
+
+
+    );
+  }
+}
+
+
+
+
+const styles = StyleSheet.create({
+  buttonLayout:{
+    margin: 15,
+    marginTop: 55,
+    paddingLeft: 10,
+    paddingRight:10,
+    padding: 5,
+  },
+  buttonLayoutBottom:{
+    marginTop: 50,
+    padding: 5,
+    backgroundColor: '#6432c8',
+  },
+  errorMessage:{
+    color: '#F44336',
+  },
+
+  inputLayout:{
+    marginRight:15,
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black'
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  buttonView:{
+    flex:1,
+    flexDirection: "row",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20
+  },
+});
--- src/Vistas/AddPrendaView.js
+++ src/Vistas/AddPrendaView.js
@@ -1,9 +1,10 @@
 /*React*/
 import React,   { Component } from 'react';
-import { StyleSheet,StatusBar,View } from 'react-native';
+import { StyleSheet,StatusBar,View,Image } from 'react-native';
 /*Components*/
 import { Container,Card,Label,Header,Form,Content,Button,Text,CardItem,Item,Body,Title,Input,Icon,Right,Left } from 'native-base';
 import { Spinner } from '../Componentes/Spinner';
+import AddElement  from '../Componentes/AddElement';
 /*Database and Auth*/
 import firebase from 'firebase';
 
@@ -18,109 +19,25 @@ export default class AddPrendaView extends Component {
     this.props.navigation.goBack();
   }
 
-  takePicture(){
-    const options={}
-    this.camera.capture({metadata:options}).then((data)=>{
-      console.log(data)
-
-    }).catch((error)=>{
-      console.log(error)
-    })
-
+  OpenCamera=(event)=>{
+    this.props.navigation.navigate("CameraView",{returnData: event})
   }
+  onPressBack = ()=>{
+    this.props.navigation.goBack();
+  }
+
+
+  addNewPrenda=(myData)=>{
+    //Aqui se agrega el metodo de agregar prenda
+    alert(myData.Nombre)
+  }
+
 
   render() {
     return (
       <Container>
-        <Header style={{backgroundColor: '#6432c8'}}>
-          <StatusBar backgroundColor={'#6432c8'} barStyle="light-content"/>
-          <Left>
-            <Button onPress={this.onPressBack} transparent>
-              <Icon name='arrow-back' />
-            </Button>
-          </Left>
-          <Body>
-            <Title>Add Clothes</Title>
-          </Body>
-        </Header>
-          <Content>
-            <Form>
-              <Card>
-                <CardItem cardBody button onPress={this.OpenCamera}>
-                  <Body style={{ flex: 1,alignItems: 'center', flexDirection: 'row',justifyContent: 'center', height: 200  }}>
-                    <Icon type="FontAwesome" name='camera-retro' />
-                  </Body>
-                </CardItem>
-              </Card>
-                <Item style ={styles.inputLayout} floatingLabel require>
-                  <Label>Name</Label>
-                  <Input value={this.props.Name} onChangeText={(info) => {this.setState({Name:info})}}/>
-                </Item>
-                <Item style ={styles.inputLayout} floatingLabel require>
-                  <Label>Description</Label>
-                  <Input value={this.props.Description} onChangeText={(info) => {this.setState({Description:info})}}/>
-                </Item>
-                <View style ={styles.buttonView}>
-                  <Button onPress= {this.addNewCategory} style={styles.buttonLayoutBottom} >
-                    <Text style={{color: 'white',fontWeight: 'bold',fontSize: 16}}>Add Category</Text>
-                  </Button>
-                </View>
-            </Form>
-          </Content>
-        </Container>
-
-
+        <AddElement Title= {'Agregar Prenda'} onPressBack={this.onPressBack} OpenCamera={this.OpenCamera} addNew={this.addNewPrenda} dataEdit={this.props.navigation.state.params.paramData} ></AddElement>
+      </Container>
     );
   }
 }
-
-
-
-
-const styles = StyleSheet.create({
-  buttonLayout:{
-    margin: 15,
-    marginTop: 55,
-    paddingLeft: 10,
-    paddingRight:10,
-    padding: 5,
-  },
-  buttonLayoutBottom:{
-    marginTop: 50,
-    padding: 5,
-    backgroundColor: '#6432c8',
-  },
-  errorMessage:{
-    color: '#F44336',
-  },
-
-  inputLayout:{
-    marginRight:15,
-  },
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black'
-  },
-  preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center'
-  },
-  buttonView:{
-    flex:1,
-    flexDirection: "row",
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    padding: 15,
-    paddingHorizontal: 20,
-    alignSelf: 'center',
-    margin: 20
-  },
-});
