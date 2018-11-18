1cdb63f499c849e28d583d7f78f75cc4f51f7a2a
Steven Iglesias <steveniglesias@hotmail.com>
2018-11-18 12:52:15 -0500

    Database Migration
    DB migration

--- src/Database/ApiKeys.js
+++ src/Database/ApiKeys.js
@@ -1,10 +1,10 @@
 export default {
   FirebaseConfig: {
-    apiKey: 'AIzaSyBZ6rxNWgjunoLZcyHDsQep-bA0rsKROFM',
-    authDomain: 'theclo-set.firebaseapp.com',
-    databaseURL: 'https://theclo-set.firebaseio.com',
-    projectId: 'theclo-set',
-    storageBucket: 'theclo-set.appspot.com',
-    messagingSenderId: '664049846660'
+    apiKey: "AIzaSyD2bMOGskmVt8stooAAT6dF7gDRQwt3nKw",
+    authDomain: "theclosetapp-4962f.firebaseapp.com",
+    databaseURL: "https://theclosetapp-4962f.firebaseio.com",
+    projectId: "theclosetapp-4962f",
+    storageBucket: "theclosetapp-4962f.appspot.com",
+    messagingSenderId: "301436960002"
   }
 }
